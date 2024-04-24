a5cd0f31617c24184ea8bb16e38f8bba26e7b9f9
Silas <silasampomah23@gmail.com>
2024-04-24 14:12:46 +0200

    aggiuntoHandle

--- taskManagementSystem.js
+++ taskManagementSystem.js
@@ -1,1 +1,28 @@
-const task = []
+const tasks = []
+function addTask(task, callback) {
+    setTimeout(() => {
+        if (typeof task !== "string" || task == "") {
+            callback(new Error("Task must be a non-empty string",null));
+        } else {
+            tasks.push(task);
+            callback(null, "Task added successfully");
+        }
+    }, 2000);
+}
+
+function exapleUsage(){
+    addTask("silas", (errore , tipo) => console.log(tipo))
+    addTask("", (errore, tipo) => console.log(errore))
+    addTask("Antonio 1",(errore, tipo) => console.log(tipo))
+    addTask("122445",(errore, tipo) => console.log(tipo))
+    CompleteTask(0, (errore, tipo)=> console.log(tipo))
+    CompleteTask(0, (errore, tipo)=> console.log(errore))
+}
+exapleUsage()
+    
+
+
+
+
+
+
